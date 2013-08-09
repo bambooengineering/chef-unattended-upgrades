64e89e0f86394ea3e2872695c5748abea7bd394d
Pablo Gutiérrez del Castillo <pablogutierrezdelc@gmail.com>
2013-08-09 16:07:01 -0300

    Added Package-Blacklist attribute

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,7 @@
 default['unattended-upgrades']['update_package_lists_interval'] = "1"
 default['unattended-upgrades']['upgrade_interval'] = "1"
 default['unattended-upgrades']['origins'] = ['${distro_id} ${distro_codename}-security']
+default['unattended-upgrades']['package-blacklist'] = []
 default['unattended-upgrades']['send_email'] = false
 default['unattended-upgrades']['email_address'] = "root@localhost"
 default['unattended-upgrades']['auto_remove'] = false
