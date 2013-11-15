e0a986d5fa1760d8f12fc2fe4c3e8076b5a2bf72
Pablo Gutiérrez del Castillo <pablogutierrezdelc@gmail.com>
2013-11-15 16:59:51 -0300

    new version

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email  "rob@firstbanco.com"
 license           "Apache 2.0"
 description       "Configures the Ubuntu unattended-upgrades package"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "1.0.0"
+version           "1.0.1"
 recipe            "unattended-upgrades", "Installs and configures the unattended-upgrades package"
 
 depends 'apt'
