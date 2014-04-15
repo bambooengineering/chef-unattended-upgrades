8cc80e964db897cb5b4890a0f56490ee83d956de
Jason Rikard <jsnrkd@gmail.com>
2014-04-15 17:22:25 -0400

    Added name to fix issue with berkshelf dependency riddly.

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,7 @@ description       "Configures the Ubuntu unattended-upgrades package"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           "1.0.0"
 recipe            "unattended-upgrades", "Installs and configures the unattended-upgrades package"
+name              "chef-unattended-upgrades"
 
 depends 'apt'
 supports 'ubuntu'
